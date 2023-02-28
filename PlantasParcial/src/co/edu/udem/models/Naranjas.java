0a01ca9c4b9517f8bb5803a85b9844fb2389ad22
Afitd_72 <samuel.afitd.canaveral@gmail.com>
2023-02-28 07:30:51 -0500

    modelos1

--- PlantasParcial/src/co/edu/udem/models/Naranjas.java
+++ PlantasParcial/src/co/edu/udem/models/Naranjas.java
@@ -9,5 +9,37 @@ package co.edu.udem.models;
  * @author b12s309
  */
 public class Naranjas {
-    
+    private String nombre;
+    private String Edad;
+    private String Prom_vida;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(String Edad) {
+        this.Edad = Edad;
+    }
+
+    public void setProm_vida(String Prom_vida) {
+        this.Prom_vida = Prom_vida;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEdad() {
+        return Edad;
+    }
+
+    public String getProm_vida() {
+        return Prom_vida;
+    }
+
+    public Naranjas(String nombre, String Edad, String Prom_vida) {
+        this.nombre = nombre;
+        this.Edad = Edad;
+        this.Prom_vida = Prom_vida;
+    }
 }
