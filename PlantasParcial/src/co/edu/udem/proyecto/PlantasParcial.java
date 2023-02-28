83a084847f23adecae3381dddb4df598e9aed7e8
Afitd_72 <samuel.afitd.canaveral@gmail.com>
2023-02-28 07:00:01 -0500

    ORIGEN

---
+++ PlantasParcial/src/co/edu/udem/proyecto/PlantasParcial.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package co.edu.udem.proyecto;
+
+/**
+ *
+ * @author b12s309
+ */
+public class PlantasParcial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
