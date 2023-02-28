853792a26fb4bc8603c69b3b18ff88904405bb8b
Afitd_72 <samuel.afitd.canaveral@gmail.com>
2023-02-28 07:31:21 -0500

    modelo2

--- PlantasParcial/src/co/edu/udem/models/Cebollas.java
+++ PlantasParcial/src/co/edu/udem/models/Cebollas.java
@@ -9,5 +9,48 @@ package co.edu.udem.models;
  * @author b12s309
  */
 public class Cebollas {
+    private String nombre;
+    private String Edad;
+    private String Prom_vida;
+    private String tipo;
+
+    public Cebollas(String nombre, String Edad, String Prom_vida, String tipo) {
+        this.nombre = nombre;
+        this.Edad = Edad;
+        this.Prom_vida = Prom_vida;
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEdad() {
+        return Edad;
+    }
+
+    public String getProm_vida() {
+        return Prom_vida;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(String Edad) {
+        this.Edad = Edad;
+    }
+
+    public void setProm_vida(String Prom_vida) {
+        this.Prom_vida = Prom_vida;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
     
 }
